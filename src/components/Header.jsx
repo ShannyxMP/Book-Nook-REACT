39902451508a8a557e6eee0bb98fc9d9d6e132f0
ShannyxMP <shannyxmp@gmail.com>
2025-09-12 23:27:43 +1000

    refactor: migrate header.ejs and footer.ejs into React components

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header id="Book-Nook">
+            <h1 id="title">
+                <a href="/">
+                <span class="material-icons-round">book</span>
+                Book Nook
+                </a>
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
