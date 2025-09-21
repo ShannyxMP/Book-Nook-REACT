e8efaea3556ec7610669098bec6574ba857dcffa
ShannyxMP <shannyxmp@gmail.com>
2025-09-21 11:00:24 +1000

    feat: add Entry.jsx & FormSegment.jsx and migrate entry.ejs features

---
+++ src/components/Entry.jsx
@@ -0,0 +1,180 @@
+import React from "react";
+import FormSegment from "./FormSegment";
+
+function Entry({
+  entryDetails,
+  showLineBreak,
+  showDatePublished,
+  showPreview,
+  showButtons,
+  showForm,
+  showEditForm,
+  showPostForm,
+}) {
+  const formattedDate = new Date(
+    entryDetails.date_created
+  ).toLocaleDateString();
+  const preview = entryDetails.review.substring(0, 300);
+
+  return (
+    <div class="entire-entry-container">
+      {/* Divider between entries */}
+      {showLineBreak && (
+        <div class="lineBreak-wrapper">
+          <hr />
+        </div>
+      )}
+
+      {/* Polaroid image showing book details */}
+      <div class="polaroid randomRotation">
+        <div class="entry-img-wrapper">
+          <img
+            src={entryDetails.book_cover}
+            alt={`book cover for ${entryDetails.title}`}
+          />
+        </div>
+
+        {showDatePublished && (
+          <div class="entry-date">
+            <p>
+              Date Created:
+              {/* If no date fetched from database, fallback to placeholder */}
+              {entryDetails.date_created ? (
+                <span>{formattedDate}</span>
+              ) : (
+                <span class="empty">--/--/----</span>
+              )}
+            </p>
+          </div>
+        )}
+
+        <div class="entry-titleAuthor">
+          <p>
+            <span>
+              <a
+                href={entryDetails.ol_link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {entryDetails.title}
+              </a>
+            </span>
+            <br />
+            by
+            {/* If no author fetched from API, fallback to placeholder */}
+            {entryDetails.author ? (
+              entryDetails.author
+            ) : (
+              <span class="empty">
+                <em>No Author yet. Please update.</em>
+              </span>
+            )}
+          </p>
+        </div>
+      </div>
+
+      {/* Section containing rating, review +/- forms and buttons, depending on flags */}
+      <div class="entry-content-container">
+        <div class="entry-rating">
+          <p>
+            {/* If no rating fetched from database, fallback to placeholder */}
+            {entryDetails.rating ? (
+              showPreview ? (
+                <>
+                  <h2> Rating </h2>
+                  <br />
+                  <div>
+                    {entryDetails.rating}
+                    <hr />5
+                  </div>
+                </>
+              ) : (
+                <div>{entryDetails.rating} / 5</div>
+              )
+            ) : (
+              <div>
+                <span class="empty">No rating yet. Fill form below.</span>
+              </div>
+            )}
+          </p>
+        </div>
+        <div class="entry-review">
+          <p>
+            {/* If no review fetched from database, fallback to placeholder */}
+            {entryDetails.review ? (
+              showPreview ? (
+                <>
+                  <h2> Review </h2>
+                  <br />
+                  {preview}...
+                  <br />
+                  <a
+                    href={`/view/${entryDetails.review_id}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span class="material-icons-round">read_more</span>
+                  </a>
+                </>
+              ) : (
+                entryDetails.review
+              )
+            ) : (
+              <span class="empty">No review yet. Fill form below.</span>
+            )}
+          </p>
+        </div>
+        {/* Home, Edit, and Delete button */}
+        {showButtons && (
+          <div class="entry-buttons">
+            {!showPreview && (
+              <a href="/">
+                <span class="material-icons-round">home</span>
+              </a>
+            )}
+            <a href={`/edit/${entryDetails.review_id}`}>
+              <span class="material-icons-round">edit</span>
+            </a>
+            <form method="POST">
+              <a
+                href={`/delete/${entryDetails.review_id}`}
+                onclick="return confirm('Are you sure you want to delete this entry?')"
+              >
+                <span class="material-icons-round">delete</span>
+              </a>
+            </form>
+          </div>
+        )}
+        {/* Show either edit or post form, depending on flags */}
+        {showForm && showEditForm && (
+          <form action={`/edit-entry/${entryDetails.review_id}`} method="POST">
+            <input type="hidden" name="isbn" value={entryDetails.book_isbn} />
+
+            <h2>Update Entry :</h2>
+            <FormSegment />
+          </form>
+        )}
+        {/* NOTE: JSON.stringify(book) was not working and resorted to approach below: --> */}
+        {showForm && showPostForm && (
+          <form action="/post-new-entry" method="POST">
+            <input type="hidden" name="bookTitle" value={entryDetails.title} />
+            <input
+              type="hidden"
+              name="bookCover"
+              value={entryDetails.book_cover}
+            />
+            <input type="hidden" name="bookISBN" value={entryDetails.isbn} />
+            <input
+              type="hidden"
+              name="bookOLlink"
+              value={entryDetails.ol_link}
+            />
+            <FormSegment />
+          </form>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Entry;
---
+++ src/components/FormSegment.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+function FormSegment() {
+  return (
+    <>
+      <div class="entry-input-author">
+        <h3>Author:</h3>
+        <input
+          type="text"
+          name="author"
+          placeholder="Author"
+          value="<%= entryDetails.author %>"
+          required
+        />
+      </div>
+
+      <div class="entry-input-rating">
+        <h3>Rating:</h3>
+        {[...Array(6)].map((_, i) => (
+          <React.Fragment key={i}>
+            <input
+              type="radio"
+              id={`rating-${i}`}
+              name="rating"
+              value={i}
+              checked={entryDetails.rating === i}
+            />
+            {/* NOTE: entryDetails.rating == i ? "checked" : ""  <- So that the user can see which rating was originally selected */}
+            <label htmlFor={`rating-${i}`}>{i}</label>
+          </React.Fragment>
+        ))}
+      </div>
+
+      <div class="entry-input-review">
+        <h3>Review:</h3>
+        <textarea
+          name="review"
+          placeholder="Write your review here..."
+          minlength="300"
+          required
+          autofocus
+        >
+          {entryDetails.review}
+        </textarea>
+      </div>
+
+      <div class="cancelSubmit-btn">
+        {/* Cancelling submission on add-entry page will refresh page, otherwise will take user to homepage */}
+        {showPostForm ? (
+          <a href="/add-entry">Cancel</a>
+        ) : (
+          <a href="/">Cancel</a>
+        )}
+        <input type="submit" value="Submit" />
+      </div>
+    </>
+  );
+}
+
+export default FormSegment;
